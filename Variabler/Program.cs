6ccbbd422d411fdaa8017c9e4822fec16304e7d0
Jesper hjarsen <geden_iph@yahoo.dk>
2018-08-23 15:41:50 +0200

    Opgave 1

--- Variabler/Program.cs
+++ Variabler/Program.cs
@@ -10,8 +10,40 @@ namespace Variabler
     {
         static void Main(string[] args)
         {
-            //testkommentar
-            //Jeg tilføjer nyt
+            int _nummer;
+            int _Nummer;
+            int nummer;
+            int AntaltMånederPrÅr;
+
+            //Dette er den meste brugte standard
+            int antalMaanederPrAar = 0;
+            AntaltMånederPrÅr = 0;
+            antalMaanederPrAar = antalMaanederPrAar + 1;
+
+            System.Int32 a1 = 1;
+            int a2 = 1;
+
+
+            Console.WriteLine(a1);
+            Console.WriteLine("a1 = " + a1);
+            Console.WriteLine($"a1 = {a1}");
+
+            int y = 10;
+
+            // instans medlem
+            Console.WriteLine(y.ToString());
+            // statisk medlem
+            Console.WriteLine(int.MaxValue);
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
         }
     }
 }
