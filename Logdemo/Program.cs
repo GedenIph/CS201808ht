a648108691fdd54d8a3e0d90300c7e6341e2eb83
Jesper hjarsen <geden_iph@yahoo.dk>
2018-08-24 15:52:02 +0200

    Endt undervisning

---
+++ Logdemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logdemo
+{
+    class Program
+    {
+        private static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+
+
+
+
+        static void Main(string[] args)
+        {
+            log.Debug("App startet");
+            try
+            {
+                string g = null;
+                g.ToString();
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+                
+            }
+
+
+        }
+    }
+}
