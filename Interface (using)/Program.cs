41361b997893c3760c4762101285fb5ad429c49b
admin <admin@TA-828-08>
2018-09-20 15:50:19 +0200

    Dag 5 slut

---
+++ Interface (using)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface__using_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader stream = System.IO.File.OpenText(@"c:\temp\dyrenavne.txt"))
+
+            {
+                while (stream.Peek() != -1)
+                {
+                    string navn = stream.ReadLine();
+                    Console.WriteLine(navn);
+                }
+                Console.ReadKey();
+
+            }
+            
+            
+            //stream.Close();
+            //stream = null;
+
+
+           
+
+        }
+    }
+}
